c4ebeb91ed6aaf19bff384cf8c08196f540381fc
Tewodros M <tedymesfin18@gmail.com>
2021-08-12 13:50:59 +0300

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mitane_frontend/presentation/pages/signup/farmer_sign_up_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: FarmerSignUp(),
+    );
+  }
+}
---
+++ lib/presentation/pages/signup/farmer_sign_up_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FarmerSignUp extends StatelessWidget {
+  const FarmerSignUp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("hello"),),
+    );
+  }
+}
