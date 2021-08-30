448301dd8f7edfbb4aace8e99aad49a1a550d456
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-31 00:12:56 +0300

    router generator file

---
+++ lib/route_generator.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:mitane_frontend/presentation/app_widget.dart';
+import 'package:mitane_frontend/presentation/home.dart';
+import 'package:mitane_frontend/priceHub/screens/price_hub_screen.dart';
+import 'package:mitane_frontend/agri_product/screens/product_display_screen.dart';
+import 'package:mitane_frontend/agri_product/screens/create_product_screen.dart';
+import 'package:mitane_frontend/presentation/price.dart';
+
+import 'package:mitane_frontend/presentation/setting.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => AppWidget());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/service':
+        return MaterialPageRoute(builder: (_) => SettingPage());
+      case '/login':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/register':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/productDetail':
+        return MaterialPageRoute(builder: (_) => SettingPage());
+
+      case '/pricehub':
+        return MaterialPageRoute(builder: (_) => PriceHub());
+      case '/products':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/machinery':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/store':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/postProduct':
+        return MaterialPageRoute(builder: (_) => CreateProductScreen());
+      case '/postInput':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/postMachinery':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/updateProduct':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/updateMachinery':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/updateInput':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/profile':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      case '/service':
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => ProductScreen());
+    }
+  }
+}
