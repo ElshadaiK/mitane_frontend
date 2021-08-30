140c7e1f11258c88af8ba9875ea29994179184a3
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-31 02:23:03 +0300

    modification

--- lib/agri_inputs/screens/ingredient_page.dart
+++ lib/agri_inputs/screens/ingredient_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mitane_frontend/presentation/pages/custome_widgets/custome_list_tile.dart';
+import 'package:mitane_frontend/presentation/pages/custome_widgets/drawer.dart';
 import 'package:mitane_frontend/presentation/pages/signup/widgets/bubbles.dart';
 
 class IngredientScreen extends StatefulWidget {
@@ -19,10 +20,7 @@ class _IngredientScreenState extends State<IngredientScreen> {
         child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.black,
-            ),
+            iconTheme: IconThemeData(color: Colors.black),
             title: Container(
               height: 35.0,
               child: TextField(
@@ -36,6 +34,7 @@ class _IngredientScreenState extends State<IngredientScreen> {
               ),
             ),
           ),
+          drawer: NavDrawer(),
           body: Stack(children: [
             Positioned(
               child: Bubble(
@@ -99,7 +98,7 @@ Widget horizontalScrollList(BuildContext context) {
                   ),
                   Center(
                     child: Text(
-                      "Crop",
+                      "Fertilizer",
                       style: TextStyle(
                           color: Colors.white,
                           fontFamily: "RobotMono",
@@ -127,10 +126,11 @@ Widget verticalScrollList(BuildContext context) {
               padding:
                   const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
               child: CustomeTile(
-                  product: "Uria",
-                  quantity: "40kg",
-                  price: "500",
-                  borderColor: Colors.green),
+                product: "Uria",
+                quantity: "40kg",
+                price: "500",
+                category: "fetilizer",
+              ),
             ),
           );
         },
