a0ac7c5e32b029a67fd0b3357ed04c59cab24068
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-31 02:25:00 +0300

    few changes

--- lib/presentation/pages/custome_widgets/custome_list_tile.dart
+++ lib/presentation/pages/custome_widgets/custome_list_tile.dart
@@ -2,74 +2,77 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
-class CustomeTile extends StatelessWidget{
+class CustomeTile extends StatelessWidget {
   final String product;
   final String quantity;
   final String price;
-  final Color borderColor;
+  final String category;
 
-  CustomeTile({Key? key,required this.product, required this.quantity, required this.price, required this.borderColor }):super(key:key);
+  CustomeTile(
+      {Key? key,
+      required this.product,
+      required this.quantity,
+      required this.price,
+      required this.category})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 3.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(topRight:Radius.circular(20),bottomRight: Radius.circular(20))
-        ),
-
-        child: Container(
-          height: 90,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            border: Border(left: BorderSide(width: 5,color: borderColor))
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20), bottomRight: Radius.circular(10))),
+      child: Container(
+        height: 90,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            border: Border(left: BorderSide(width: 5, color: Colors.green))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "$product",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "RobotMono"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      "$category",
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text(
-                        "$product",
-                        style: TextStyle(
-                          fontSize: 25
+                    Row(
+                      children: [
+                        FaIcon(FontAwesomeIcons.dollarSign),
+                        Text(
+                          '$price',
+                          style: TextStyle(fontSize: 25),
                         ),
-                      ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      child: Text(
-                            "Vegitable",
-                            textAlign: TextAlign.start,
-                        ),
+                      ],
                     ),
-                    
+                    Text('$quantity')
                   ],
-                ),
-              ),
-              Text(
-                "Quantity $quantity"
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 25),
-                child: Row(
-                  children: [
-                    FaIcon(FontAwesomeIcons.dollarSign),
-                    Text(
-                      '$price',
-                      style: TextStyle(
-                        fontSize: 25
-                      ),
-                    ),
-                  ],
-                )
-              )
-            ],
-          ),
+                ))
+          ],
         ),
-      );
+      ),
+    );
   }
 }
