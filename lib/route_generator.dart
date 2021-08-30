de834fa8ebd5d75903c1de03a1a4c206a0f4b673
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-31 02:20:25 +0300

    bug fixed

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mitane_frontend/agri_inputs/inputs.dart';
+import 'package:mitane_frontend/machinery/screens/machinery_screen.dart';
 import 'package:mitane_frontend/presentation/app_widget.dart';
 import 'package:mitane_frontend/presentation/home.dart';
 import 'package:mitane_frontend/priceHub/screens/price_hub_screen.dart';
@@ -13,23 +15,19 @@ class RouteGenerator {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => AppWidget());
-      case '/home':
-        return MaterialPageRoute(builder: (_) => ProductScreen());
+
       case '/login':
         return MaterialPageRoute(builder: (_) => ProductScreen());
       case '/register':
         return MaterialPageRoute(builder: (_) => ProductScreen());
-      case '/productDetail':
-        return MaterialPageRoute(builder: (_) => HomePage());
-
       case '/pricehub':
         return MaterialPageRoute(builder: (_) => PriceHub());
-      case '/products':
+      case '/product':
         return MaterialPageRoute(builder: (_) => ProductScreen());
       case '/machinery':
-        return MaterialPageRoute(builder: (_) => ProductScreen());
-      case '/store':
-        return MaterialPageRoute(builder: (_) => ProductScreen());
+        return MaterialPageRoute(builder: (_) => MachineryScreen());
+      case '/inputs':
+        return MaterialPageRoute(builder: (_) => IngredientScreen());
       case '/postProduct':
         return MaterialPageRoute(builder: (_) => CreateProductScreen());
       case '/postInput':
