9717479a50867695375b266506a44aa5b0ff3281
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-08-31 02:22:44 +0300

    modification

--- lib/agri_product/screens/product_display_screen.dart
+++ lib/agri_product/screens/product_display_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mitane_frontend/presentation/pages/custome_widgets/custome_list_tile.dart';
+import 'package:mitane_frontend/presentation/pages/custome_widgets/drawer.dart';
 import 'package:mitane_frontend/presentation/pages/signup/widgets/bubbles.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -19,10 +20,7 @@ class _ProductScreenState extends State<ProductScreen> {
         child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.white,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.black,
-            ),
+            iconTheme: IconThemeData(color: Colors.black),
             title: Container(
               height: 35.0,
               child: TextField(
@@ -36,6 +34,7 @@ class _ProductScreenState extends State<ProductScreen> {
               ),
             ),
           ),
+          drawer: NavDrawer(),
           body: Stack(children: [
             Positioned(
               child: Bubble(
@@ -127,10 +126,11 @@ Widget verticalScrollList(BuildContext context) {
               padding:
                   const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
               child: CustomeTile(
-                  product: "Wheat",
-                  quantity: "100Kg",
-                  price: "300",
-                  borderColor: Colors.green),
+                product: "Wheat",
+                quantity: "100Kg",
+                price: "300",
+                category: "crop",
+              ),
             ),
           );
         },
