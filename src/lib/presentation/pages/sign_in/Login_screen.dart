c5878c571c21f34298f8d39dc93b41302f5f0887
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-09-09 18:14:57 +0300

    some modefications

--- src/lib/presentation/pages/sign_in/Login_screen.dart
+++ src/lib/presentation/pages/sign_in/Login_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mitane_frontend/application/auth/bloc/auth_bloc.dart';
