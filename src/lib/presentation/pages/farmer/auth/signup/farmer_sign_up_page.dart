8197aa9d1fc668cf4ff088b502fd08b76cbc372d
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-09-09 21:26:30 +0300

    register authentication

--- src/lib/presentation/pages/farmer/auth/signup/farmer_sign_up_page.dart
+++ src/lib/presentation/pages/farmer/auth/signup/farmer_sign_up_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mitane_frontend/application/auth/bloc/auth_bloc.dart';
+import 'package:mitane_frontend/application/auth/events/auth_events.dart';
+import 'package:mitane_frontend/application/auth/states/auth_state.dart';
+import 'package:mitane_frontend/domain/auth/entity/auth_model.dart';
 import 'package:mitane_frontend/presentation/pages/custom_widgets/widgets/bubbles.dart';
 import 'package:mitane_frontend/presentation/pages/farmer/auth/signup/farmer_sign_up2_page.dart';
-
 
 class FarmerSignUp extends StatefulWidget {
   FarmerSignUp({Key? key}) : super(key: key);
@@ -9,9 +13,14 @@ class FarmerSignUp extends StatefulWidget {
   @override
   _FarmerSignUpState createState() => _FarmerSignUpState();
 }
+
 class _FarmerSignUpState extends State<FarmerSignUp> {
-  final myController = TextEditingController();
-  int _value = 1;
+  final phoneController = TextEditingController();
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmController = TextEditingController();
+  int _role = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,173 +42,202 @@ class _FarmerSignUpState extends State<FarmerSignUp> {
             top: 120,
             left: 180,
           ),
-          Positioned(
-            child: Text(
-              "Please fill your \ninformation",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            top: 150,
-            left: 50,
-          ),
-          Positioned(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              padding: EdgeInsets.all(20),
-              child: DropdownButton(
-                value: _value,
-                items: [
-                  DropdownMenuItem(
-                    child: Text("Farmer"),
-                    value: 1,
+          Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Please fill your \ninformation",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
                   ),
-                  DropdownMenuItem(
-                    child: Text("Accessory Trader"),
-                    value: 2,
+                ),
+                SizedBox(
+                  width: 240,
+                  child: DropdownButton(
+                    value: _role,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Farmer"),
+                        value: 1,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Accessory Trader"),
+                        value: 2,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Product Trader"),
+                        value: 3,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Tool Trader"),
+                        value: 4,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("User"),
+                        value: 5,
+                      ),
+                    ],
+                    onChanged: (int? value) {
+                      setState(() {
+                        _role = value!;
+                      });
+                    },
+                    isExpanded: true,
                   ),
-                  DropdownMenuItem(
-                    child: Text("Product Trader"),
-                    value: 3,
-                  ),
-                  DropdownMenuItem(
-                    child: Text("Tool Trader"),
-                    value: 4,
-                  ),
-                ],
-                onChanged: (int? value) {
-                  setState(() {
-                    _value = value!;
-                  });
-                },
-                isExpanded: true,
-              ),
-            ),
-            top: 200,
-            left: MediaQuery.of(context).size.width * 0.2,
-          ),
-          Positioned(
-            child: Container(
-              width: 60,
-              child: Material(
-                elevation: 2,
-                borderRadius: BorderRadius.circular(5),
-                child: TextField(
-                  controller: TextEditingController()..text = '+251',
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide.none,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 60,
+                      child: Material(
+                        elevation: 2,
+                        borderRadius: BorderRadius.circular(5),
+                        child: TextField(
+                          controller: TextEditingController()..text = '+251',
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      width: 230,
+                      child: Material(
+                        elevation: 2,
+                        borderRadius: BorderRadius.circular(5),
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          autocorrect: false,
+                          controller: phoneController,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: '(###) ##-####'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 300,
+                  child: Material(
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(5),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Name'),
                     ),
                   ),
                 ),
-              ),
-            ),
-            top: 280,
-            left: 30,
-          ),
-          Positioned(
-            child: Container(
-              width: 220,
-              child: Material(
-                elevation: 2,
-                borderRadius: BorderRadius.circular(5),
-                child: TextField(
-                  keyboardType: TextInputType.phone,
-                  autocorrect: false,
-                  controller: myController,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintText: '(###) ###-####'),
-                ),
-              ),
-            ),
-            top: 280,
-            left: 100,
-          ),
-          Positioned(
-            child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FarmerSignUp2()),
-                  );
-                }, // handle your onTap here
-                child: Container(
-                  height: 46,
-                  width: 174,
-                  child: Align(
-                      child: Text(
-                    'Next',
-                    style: TextStyle(color: Colors.white),
-                  )),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(28),
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color(0x8CC63E).withOpacity(1),
-                          Color(0x709E2F).withOpacity(1),
-                        ]),
+                Container(
+                  width: 300,
+                  child: Material(
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(5),
+                    child: TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Password'),
+                    ),
                   ),
-                )),
-            top: 480,
-            left: 90,
-          ),
-          Positioned(
-            child: Container(
-              width: 300,
-              child: Material(
-                elevation: 2,
-                borderRadius: BorderRadius.circular(5),
-                child: TextField(
-                  controller: myController,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintText: 'Name'),
                 ),
-              ),
-            ),
-            top: 350,
-            left: 30,
-          ),
-          Positioned(
-            child: Container(
-              width: 300,
-              child: Material(
-                elevation: 2,
-                borderRadius: BorderRadius.circular(5),
-                child: TextField(
-                  controller: myController,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintText: 'Password'),
+                Container(
+                  width: 300,
+                  child: Material(
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(5),
+                    child: TextField(
+                      controller: confirmController,
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Confirm password'),
+                    ),
+                  ),
                 ),
-              ),
+                BlocConsumer<AuthBloc, AuthState>(builder: (context, state) {
+                  Widget button = Align(
+                      child: Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ));
+                  if (state is Registering) {
+                    button = Center(
+                        child: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: CircularProgressIndicator(),
+                    ));
+                  }
+
+                  return InkWell(
+                      onTap: () {
+                        final authBloc = context.read<AuthBloc>();
+                        authBloc.add(RegisterEvent(
+                            register: (Register(
+                                name: nameController.text,
+                                phone: phoneController.text,
+                                password: passwordController.text,
+                                confirm: confirmController.text,
+                                role: _role.toString()))));
+                      }, // handle your onTap here
+                      child: Container(
+                        height: 46,
+                        width: 174,
+                        child: button,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(0x8CC63E).withOpacity(1),
+                                Color(0x709E2F).withOpacity(1),
+                              ]),
+                        ),
+                      ));
+                }, listener: (context, state) {
+                  if (state is RegisterSuccess) {
+                    Navigator.pushNamed(context, '/login');
+                  }
+                })
+              ],
             ),
-            top: 410,
-            left: 30,
-          ),
+          )
         ],
       ),
     );
