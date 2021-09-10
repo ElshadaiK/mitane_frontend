b646a31a98b4286c5a784e936dcaa746c6f87448
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2021-09-11 02:26:48 +0300

    ingredient testing updated

--- src/test/ingredient/bloc_test.dart
+++ src/test/ingredient/bloc_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:dio/dio.dart';
-
+import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mitane_frontend/application/ingredient/states/ingredient_state.dart';
 import 'package:mitane_frontend/infrastructure/ingredient/data_provider/ingredient_provider.dart';
@@ -8,42 +8,74 @@ import 'package:mitane_frontend/application/ingredient/events/ingredient_event.d
 import 'package:mitane_frontend/application/ingredient/bloc/ingredient_bloc.dart';
 import 'package:mitane_frontend/infrastructure/ingredient/repository/ingredient_repository.dart';
 import 'package:mitane_frontend/domain/ingredient/entity/ingredient_model.dart';
-import 'package:test/test.dart';
+
+import 'package:mocktail/mocktail.dart';
 
 class MockIngredientBloc extends MockBloc<IngredientEvent, IngredientState>
     implements IngredientBloc {}
 
-class MockIngredientRepository
-    extends MockBloc<IngredientEvent, IngredientState>
-    implements IngredientRepository {}
+class MockIngredientRepository extends Mock implements IngredientRepository {}
 
 void main() {
-  group('GetIngredient', () {
-    MockIngredientRepository ingredientRepository = MockIngredientRepository();
-    IngredientBloc ingredientBloc =
-        IngredientBloc(ingredientRepository: ingredientRepository);
-    final ingredient = Ingredient(id: "1", name: "Teff", category: "Crop");
-    setUp(() {
-      ingredientRepository = MockIngredientRepository();
-      ingredientBloc =
-          IngredientBloc(ingredientRepository: ingredientRepository);
-    });
+  MockIngredientRepository ingredientRepository = MockIngredientRepository();
+  // IngredientBloc ingredientBloc =
+  //     IngredientBloc(ingredientRepository: ingredientRepository);
+  final ingredient = Ingredient(id: "1", name: "Teff", category: "Crop");
+  Future<List<Ingredient>> createIngredients() async {
+    return [Ingredient(id: "1", name: "Teff", category: "Crop")];
+  }
 
-    blocTest<IngredientBloc, IngredientState>(
-        'emits [IngredientLoading,IngredientAdminOperationSuccess] when successful',
-        build: () {
-          final ingredients = ingredientRepository.fetchAll();
-          final Dio dio = Dio();
+  Future<Ingredient> createIngredient() async {
+    return Ingredient(id: "1", name: "Teff", category: "Crop");
+  }
 
-          final IngredientRepository ingredientRepo = IngredientRepository(
-              dataProvider: IngredientDataProvider(dio: dio));
-          return IngredientBloc(ingredientRepository: ingredientRepo);
-        },
-        act: (bloc) => bloc.add(IngredientAdminLoad()),
-        expect: [
-          IngredientAdminLoading(),
-          IngredientAdminOperationSuccess(ingredient),
-          IngredientAdminOperationError()
-        ]);
+  ;
+
+  setUp(() {
+    ingredientRepository = MockIngredientRepository();
+    // ingredientBloc =
+    //     IngredientBloc(ingredientRepository: ingredientRepository);
   });
+
+  blocTest<IngredientBloc, IngredientState>(
+      'emits [IngredientLoading,IngredientAdminOperationSuccess] when successful',
+      build: () {
+        when(() => ingredientRepository.fetchAll())
+            .thenAnswer((invocation) => createIngredients());
+        return IngredientBloc(ingredientRepository: ingredientRepository);
+      },
+      act: (bloc) => bloc.add(IngredientAdminLoad()),
+      expect: () {
+        return [
+          IngredientAdminLoading(),
+          isA<IngredientAdminOperationSuccess>()
+        ];
+      });
+
+  blocTest<IngredientBloc, IngredientState>(
+      'emits [IngredientLoading,IngredientAdminOperationFailure] when unsuccessful',
+      build: () {
+        return IngredientBloc(ingredientRepository: ingredientRepository);
+      },
+      act: (bloc) => bloc.add(IngredientAdminLoad()),
+      expect: () {
+        return [IngredientAdminLoading(), IngredientAdminOperationFailure()];
+      });
+
+  blocTest<IngredientBloc, IngredientState>(
+      'emits [IngredientLoading,IngredientAdminOperationSuccess] when successful',
+      build: () {
+        when(() => ingredientRepository.create(ingredient))
+            .thenAnswer((invocation) => createIngredient());
+        when(() => ingredientRepository.fetchAll())
+            .thenAnswer((invocation) => createIngredients());
+        return IngredientBloc(ingredientRepository: ingredientRepository);
+      },
+      act: (bloc) => bloc.add(IngredientAdminCreate(ingredient)),
+      expect: () {
+        return [
+         
+          isA<IngredientAdminOperationSuccess>()
+        ];
+      });
 }
--- src/test/ingredient/widget_test.dart
+++ src/test/ingredient/widget_test.dart
@@ -3,8 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mitane_frontend/presentation/pages/agri_inputs/screens/ingredient_page.dart';
 
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
+  testWidgets('It has an appbar', (WidgetTester tester) async {
+   
     final childWidget = AppBar(
         backgroundColor: Colors.blue,
         iconTheme: IconThemeData(color: Colors.black));
