bc9a3a64a2ac78e7eb84e2add8b134c79f9d84c4
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2021-09-11 22:44:45 +0300

    Ingredient and auth testing done

--- src/test/ingredient/widget_test.dart
+++ src/test/ingredient/widget_test.dart
@@ -4,11 +4,26 @@ import 'package:mitane_frontend/presentation/pages/agri_inputs/screens/ingredien
 
 void main() {
   testWidgets('It has an appbar', (WidgetTester tester) async {
-   
-    final childWidget = AppBar(
-        backgroundColor: Colors.blue,
-        iconTheme: IconThemeData(color: Colors.black));
-    await tester.pumpWidget(Scaffold(appBar: childWidget));
+    await tester.pumpWidget(IngredientScreen());
+    final childWidget = Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.blue,
+            iconTheme: IconThemeData(color: Colors.black)));
+
     expect(find.byWidget(childWidget), findsOneWidget);
+  });
+
+  testWidgets('Ingredient page contains 1 input fields',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(IngredientScreen());
+    final inputFieldsCount = find.byType(TextField);
+    expect(inputFieldsCount, findsOneWidget);
+  });
+
+  testWidgets('Ingredients page is visibile', (WidgetTester tester) async {
+    await tester.pumpWidget(IngredientScreen());
+    final infoFinder = find.text("Fertilizer");
+
+    expect(infoFinder, findsOneWidget);
   });
 }
